0e508ca4adaf95a3ba2fb42742c857157910ddde
LMontes94 <monteslucas1994@gmail.com>
2024-02-21 23:27:00 -0300

    lesson seed,factory y fake terminado, comienzo de la lesson relaciones mas txt de apuntes

---
+++ databases/app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index():View{
+        $products = Product::all();
+        return view('product.index',compact('products'));
+    }
+}
